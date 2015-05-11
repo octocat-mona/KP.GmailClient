8c7044d045d98eca0e88f796a916e1538cbb6032
K.P. Stolk <kpstolk@gmail.com>
2015-05-11 20:39:05 +0200

    Updated build script to include symbols (pdb) in Nuget package.

--- Build/Build.cmd
+++ Build/Build.cmd
@@ -1,1 +1,18 @@
-nuget pack ..\Src\KP.GmailApi\KP.GmailApi.csproj -Prop Configuration=Release -sym
+@ECHO off
+
+IF NOT DEFINED NugetApiKey (
+	ECHO Nuget API key Environment variable 'NugetApiKey' not found!
+	Exit /B 1
+)
+
+ECHO Creating Nuget package(s).
+nuget pack ..\Src\KP.GmailApi\KP.GmailApi.csproj -Prop Configuration=Release -Symbols -NonInteractive
+
+ECHO Setting Nuget API key.
+nuget setApiKey -Verbosity quiet -NonInteractive %NugetApiKey%
+
+ECHO Publishing to Nuget / SymbolSource.
+nuget push KP.GmailApi.*.nupkg
+
+ECHO done.
+@ECHO on
