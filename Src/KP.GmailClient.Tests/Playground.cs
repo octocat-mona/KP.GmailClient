cc27df361a9161a087e0ab366013cf4b4c06c702
Kapé <kpstolk@outlook.com>
2018-03-08 21:58:19 +0100

    Added using for sample Playground

--- Src/KP.GmailClient.Tests/Playground.cs
+++ Src/KP.GmailClient.Tests/Playground.cs
@@ -3,13 +3,12 @@ using System.Threading.Tasks;
 using KP.GmailClient.Models;
 using KP.GmailClient.Services.Extensions;
 using KP.GmailClient.Tests.IntegrationTests;
-using Xunit;
 
 namespace KP.GmailClient.Tests
 {
     public class Playground
     {
-        public async Task Play()
+        public async Task PlayAsync()
         {
             // ----------------------
             // --- SETUP ---
@@ -21,43 +20,44 @@ namespace KP.GmailClient.Tests
             string tokenUri = SettingsManager.GetTokenUri();
             string clientEmail = SettingsManager.GetClientEmail();
             string emailAddress = SettingsManager.GetEmailAddress();
-            ServiceAccountCredential accountCredential = new ServiceAccountCredential
+            var accountCredential = new ServiceAccountCredential
             {
                 PrivateKey = privateKey,
                 TokenUri = tokenUri,
                 ClientEmail = clientEmail
             };
-            var client = new GmailClient(accountCredential, emailAddress, scopes);
 
-            /*// Or use from downloaded JSON file directly
-            const string path = "C:\\Users\\Me\\Documents\\Gmail-Project.json";
-            var initializer = GmailClientInitializer.Initialize(path, scopes);
-            client = new GmailClient(initializer, emailAddress);*/
+            using (var client = new GmailClient(accountCredential, emailAddress, scopes))
+            {
+                /*// Or use from downloaded JSON file directly
+                const string path = "C:\\Users\\Me\\Documents\\Gmail-Project.json";
+                var initializer = GmailClientInitializer.Initialize(path, scopes);
+                client = new GmailClient(initializer, emailAddress);*/
 
-            // ----------------------
-            // --- USAGE EXAMPLES ---
-            // ----------------------
-            // Send a plain text email
-            Message sentMessage = await client.Messages.SendAsync(emailAddress, "The subject", "Plain text body");
+                // ----------------------
+                // --- USAGE EXAMPLES ---
+                // ----------------------
+                // Send a plain text email
+                Message sentMessage = await client.Messages.SendAsync(emailAddress, "The subject", "Plain text body");
 
-            // Send a HTML email
-            sentMessage = await client.Messages.SendAsync(emailAddress, "The subject", "<h1>HTML body</h1>", isBodyHtml: true);
+                // Send a HTML email
+                sentMessage = await client.Messages.SendAsync(emailAddress, "The subject", "<h1>HTML body</h1>", isBodyHtml: true);
 
-            // Get the users profile
-            Profile profile = await client.GetProfileAsync();
+                // Get the users profile
+                Profile profile = await client.GetProfileAsync();
 
-            // Get inbox messages
-            IList<Message> messages = await client.Messages.ListAsync();
+                // Get inbox messages
+                IList<Message> messages = await client.Messages.ListAsync();
 
-            // Get starred messages
-            IList<Message> starredMessages = await client.Messages.ListByLabelAsync(Label.Starred);
+                // Get starred messages
+                IList<Message> starredMessages = await client.Messages.ListByLabelAsync(Label.Starred);
 
-            // List all labels
-            IList<Label> labels = await client.Labels.ListAsync();
+                // List all labels
+                IList<Label> labels = await client.Labels.ListAsync();
 
-            // List all drafts
-            IList<Draft> drafts = await client.Drafts.ListAsync();
-
+                // List all drafts
+                IList<Draft> drafts = await client.Drafts.ListAsync();
+            }
         }
     }
 }
