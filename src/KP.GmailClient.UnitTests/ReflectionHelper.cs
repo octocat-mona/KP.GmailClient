c0dff4bd7791693bb53de23d104596e027ac7907
octocat <538170+octocat-mona@users.noreply.github.com>
2025-11-08 22:41:39 +0100

    Removed unused test helper methods.

--- src/KP.GmailClient.UnitTests/ReflectionHelper.cs
+++ src/KP.GmailClient.UnitTests/ReflectionHelper.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using System.Reflection;
 
 namespace KP.GmailClient.UnitTests
@@ -14,12 +13,6 @@ namespace KP.GmailClient.UnitTests
             BindingFlags.Public |
             BindingFlags.Default;
 
-        public static T GetFieldValue<T>(object objectInstance, string fieldName)
-        {
-            var field = GetFieldName<T>(objectInstance, fieldName);
-            return (T)field.GetValue(objectInstance);
-        }
-
         public static T GetStaticFieldValue<T>(Type objecType, string fieldName)
         {
             FieldInfo field = objecType.GetField(fieldName, Flags);
@@ -28,37 +21,7 @@ namespace KP.GmailClient.UnitTests
                 throw new Exception();
             }
 
-            return (T)field.GetValue(null);
+            return (T) field.GetValue(null);
         }
-
-        public static void SetFieldValue<T>(object objectInstance, string fieldName, object value)
-        {
-            var field = GetFieldName<T>(objectInstance, fieldName);
-            field.SetValue(objectInstance, value);
-        }
-
-        private static FieldInfo GetFieldName<T>(object objectInstance, string fieldName)
-        {
-            FieldInfo field = objectInstance.GetType().GetField(fieldName, Flags);
-            if (field == null)
-            {
-                throw new Exception($"Could not find field with name '{fieldName}'");
-            }
-            return field;
-        }
-
-        /// <summary>
-        /// Get the path of the executing assembly.
-        /// Use this to get the reliable location also for the unit test runner.
-        /// </summary>
-        /// <returns></returns>
-        public static string GetExecutingAssemblyPath()
-        {
-            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
-            UriBuilder uri = new UriBuilder(codeBase);
-            string path = Uri.UnescapeDataString(uri.Path);
-            return Path.GetDirectoryName(path);
-        }
-
     }
 }
